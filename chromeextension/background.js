f415037a7262b2eb6c72c74fe526099ae0dc03c7
wolfter12 <wolfter12@gmail.com>
2020-07-19 22:13:17 +0300

    Added button to the bottom menu

---
+++ chromeextension/background.js
@@ -0,0 +1,1 @@
+// TODO: Add enable/disable functionality via popup.html and popup.js
--- chromeextension/content.js
+++ chromeextension/content.js
@@ -1,13 +1,22 @@
 "use strict";
 
 function showButton() {
-    let markAsRead = '<li id=\"mark-as-read\" class=\"menu-item\"><a class=\"menu-link msglist-msg-read\" href=\"#\" title=\"Позначити як прочитані\">Позначити як прочитані</a></li>';
+    const markAsRead = '<li class=\"menu-item mark-as-read\"><a class=\"menu-link msglist-msg-read\" href=\"#\" title=\"Позначити як прочитані\">Позначити як прочитані</a></li>';
 
     setInterval(function() {
-        // Checking if there is a menu bar and checking if there is no button "Позначені як прочитані".
-        if (document.querySelector("#msglist-page > div.controls.controls-top > ul") && !document.querySelector("#mark-as-read")) {
-            document.querySelector("#msglist-page > div.controls.controls-top > ul").insertAdjacentHTML("beforeend", markAsRead);
-            document.querySelector("#msglist-page > div.controls.controls-top > ul").click();
+        // Checking if there is a menu bar and checking if there is no button "Позначені як прочитані"(Mark as read).
+        const topMenu = document.querySelector("#msglist-page > div.controls.controls-top > ul");
+        const bottomMenu = document.querySelector("#msglist-page > div.controls.controls-bottom > ul");
+        // check the top menu
+        if (topMenu && !topMenu.querySelector(".mark-as-read")) {
+            topMenu.insertAdjacentHTML("beforeend", markAsRead);
+            // re-initialize menu. As a result the button has an event listener
+            topMenu.click();
+        }
+         // check the bottom menu
+        if (bottomMenu && !bottomMenu.querySelector(".mark-as-read")) {
+            bottomMenu.insertAdjacentHTML("beforeend", markAsRead);
+            bottomMenu.click();
         }
     }, 100);
 }
