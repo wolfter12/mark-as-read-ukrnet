ab315e175c1e4b14444d5e2dacf318ae7323d0ea
wolfter12 <wolfter12@gmail.com>
2020-07-20 00:19:17 +0300

    Add new 'content.js' with mutation observer

--- chromeextension/content.js
+++ chromeextension/content.js
@@ -1,24 +1,40 @@
 "use strict";
 
-function showButton() {
-    const markAsRead = '<li class=\"menu-item mark-as-read\"><a class=\"menu-link msglist-msg-read\" href=\"#\" title=\"Позначити як прочитані\">Позначити як прочитані</a></li>';
+const markAsRead = '<li class=\"menu-item mark-as-read\"><a class=\"menu-link msglist-msg-read\" href=\"#\" title=\"Позначити як прочитані\">Позначити як прочитані</a></li>';
+const targetNode = document.querySelector("#msglist-page");
+const observerOptions = {
+    childList: true,
+    subtree: true
+};
+const observer = new MutationObserver(onMutation);
 
-    setInterval(function() {
-        // Checking if there is a menu bar and checking if there is no button "Позначені як прочитані"(Mark as read).
-        const topMenu = document.querySelector("#msglist-page > div.controls.controls-top > ul");
-        const bottomMenu = document.querySelector("#msglist-page > div.controls.controls-bottom > ul");
-        // check the top menu
-        if (topMenu && !topMenu.querySelector(".mark-as-read")) {
-            topMenu.insertAdjacentHTML("beforeend", markAsRead);
-            // re-initialize menu. As a result the button has an event listener
-            topMenu.click();
+function onMutation(mutations) {
+    const menus = [];
+    for (let mutation of mutations) {
+        for (let node of mutation.addedNodes) {
+            if (!node.tagName) continue; // not an element
+            // Check if .menu exists and .mark-as-read does not
+            if (node.classList.contains("menu") && !node.querySelector(".mark-as-read")) {
+                menus.push(node);
+            }
         }
-         // check the bottom menu
-        if (bottomMenu && !bottomMenu.querySelector(".mark-as-read")) {
-            bottomMenu.insertAdjacentHTML("beforeend", markAsRead);
-            bottomMenu.click();
-        }
-    }, 100);
+    }
+    if (menus.length) {
+        // stop observing, this is required to prevent a recursive function call
+        disableObserver();
+        menus.forEach(menu => menu.insertAdjacentHTML("beforeend", markAsRead));
+        // re-initialize menu. As a result the button has an event listener
+        document.querySelector("#msglist-page > div.controls.controls-top > ul").click();
+        enableObserver();
+    }
 }
 
-window.addEventListener ("load", showButton, false);
+function disableObserver() {
+    observer.disconnect();
+}
+
+function enableObserver() {
+    observer.observe(targetNode, observerOptions);
+}
+
+enableObserver();
