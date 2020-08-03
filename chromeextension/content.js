9ef00ac57167e7130b33d790661c3b9e56bacb6c
wolfter12 <wolfter12@gmail.com>
2020-08-04 00:29:12 +0300

    updated according to the eslint rules

--- chromeextension/content.js
+++ chromeextension/content.js
@@ -1,40 +1,39 @@
-"use strict";
-
-const markAsRead = '<li class=\"menu-item mark-as-read\"><a class=\"menu-link msglist-msg-read\" href=\"#\" title=\"Позначити як прочитані\">Позначити як прочитані</a></li>';
+// eslint-disable-next-line quotes
+const markAsRead = `<li class="menu-item mark-as-read"><a class="menu-link msglist-msg-read" href="#" title="Позначити як прочитані">Позначити як прочитані</a></li>`;
 const targetNode = document.querySelector("#msglist-page");
 const observerOptions = {
-    childList: true,
-    subtree: true
+  childList: true,
+  subtree: true,
 };
+// eslint-disable-next-line no-use-before-define
 const observer = new MutationObserver(onMutation);
 
-function onMutation(mutations) {
-    const menus = [];
-    for (let mutation of mutations) {
-        for (let node of mutation.addedNodes) {
-            if (!node.tagName) continue; // not an element
-            // Check if .menu exists and .mark-as-read does not
-            if (node.classList.contains("menu") && !node.querySelector(".mark-as-read")) {
-                menus.push(node);
-            }
-        }
-    }
-    if (menus.length) {
-        // stop observing, this is required to prevent a recursive function call
-        disableObserver();
-        menus.forEach(menu => menu.insertAdjacentHTML("beforeend", markAsRead));
-        // re-initialize menu. As a result the button has an event listener
-        document.querySelector("#msglist-page > div.controls.controls-top > ul").click();
-        enableObserver();
-    }
-}
-
 function disableObserver() {
-    observer.disconnect();
+  observer.disconnect();
 }
 
 function enableObserver() {
-    observer.observe(targetNode, observerOptions);
+  observer.observe(targetNode, observerOptions);
+}
+
+function onMutation(mutations) {
+  const menus = [];
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach((node) => {
+      if (!node.tagName) return;
+      if (node.classList.contains("menu") && !node.querySelector(".mark-as-read")) {
+        menus.push(node);
+      }
+    });
+  });
+  if (menus.length) {
+    // need to prevent a recursive function call
+    disableObserver();
+    menus.forEach((menu) => menu.insertAdjacentHTML("beforeend", markAsRead));
+    // re-initialize menu. As a result the button has an event listener
+    document.querySelector("#msglist-page > div.controls.controls-top > ul").click();
+    enableObserver();
+  }
 }
 
 enableObserver();
